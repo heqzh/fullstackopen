75429a01415653e2b101ebccd014d87014ea171e
hqz3 <heqzheng@gmail.com>
2022-05-02 17:40:11 -0400

    Handle errors when adding a new entry using the backend

--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -22,56 +22,52 @@ export const PersonForm = ({ persons, setPersons, setNotification }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (newName.length && newNumber.length) {
-      let lowerCaseName = newName.toLowerCase();
-      // Find the person's entry
-      let findPerson = persons.find(
-        (person) => person.name.toLowerCase() === lowerCaseName
+    let lowerCaseName = newName.toLowerCase();
+    // Find the person's entry
+    let findPerson = persons.find(
+      (person) => person.name.toLowerCase() === lowerCaseName
+    );
+    if (findPerson) {
+      let update = window.confirm(
+        `${newName} is already added to phonebook, replace the old number with a new one?`
       );
-      if (findPerson) {
-        let update = window.confirm(
-          `${newName} is already added to phonebook, replace the old number with a new one?`
-        );
-        if (!update) return;
-        // Update the found person's phone number
-        let updatedPerson = { ...findPerson, number: newNumber };
-        // PUT request the updated entry to the backend
-        phonebookServices
-          .update(findPerson.id, updatedPerson)
-          .then((updatedEntry) => {
-            // Returns a response of the updated entry
-            // Update the frontend
-            setPersons(
-              persons.map((person) =>
-                person.id !== updatedEntry.id ? person : updatedEntry
-              )
-            );
-            showNotification(
-              `Updated ${updatedEntry.name}'s number to ${updatedEntry.number}`,
-              true
-            );
-          })
-          .catch((error) => {
-            showNotification(
-              `Information for ${updatedPerson.name} has already been removed from the server`,
-              false
-            );
-          });
-      } else {
-        // POST request the new person's entry to the backend
-        phonebookServices
-          .create({ name: newName, number: newNumber })
-          .then((newEntry) => {
-            setPersons(persons.concat(newEntry));
-            showNotification(`Added ${newEntry.name}`, true);
-          })
-          .catch((error) => {
-            showNotification(error.response.data.message, false);
-          });
-      }
-      setNewName("");
-      setNewNumber("");
+      if (!update) return;
+      // Update the found person's phone number
+      let updatedPerson = { ...findPerson, number: newNumber };
+      // PUT request the updated entry to the backend
+      phonebookServices
+        .update(findPerson.id, updatedPerson)
+        .then((updatedEntry) => {
+          // Returns a response of the updated entry
+          setPersons(
+            persons.map((person) =>
+              person.id !== updatedEntry.id ? person : updatedEntry
+            )
+          );
+          showNotification(
+            `Updated ${updatedEntry.name}'s number to ${updatedEntry.number}`,
+            true
+          );
+        })
+        .catch((error) => {
+          showNotification(error.response.data.message, false);
+        });
     }
+    // If the person does not exist, create a new entry
+    else {
+      // POST request the new person's entry to the backend
+      phonebookServices
+        .create({ name: newName, number: newNumber })
+        .then((newEntry) => {
+          setPersons(persons.concat(newEntry));
+          showNotification(`Added ${newEntry.name}`, true);
+        })
+        .catch((error) => {
+          showNotification(error.response.data.message, false);
+        });
+    }
+    setNewName("");
+    setNewNumber("");
   };
 
   return (
